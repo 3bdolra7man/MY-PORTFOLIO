47cb9fe4b367edb508df2d182756dd208ea787ac
Abdelrahman Faiz <3bdelra7mane.g@gmail.com>
2025-07-03 10:08:05 +0300

    main.js

--- JS/main.js
+++ JS/main.js
@@ -27,9 +27,6 @@ window.onscroll = () => {
     let header = document.querySelector('header')
     header.classList.toggle('sticky',window.scrollY> 100);
 
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
 };
 
 ScrollReveal({
